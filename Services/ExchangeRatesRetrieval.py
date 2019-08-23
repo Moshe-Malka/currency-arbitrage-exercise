8b94b6e736a9d3dec03daae0512463603d0a65fd
Moshe-Malka <moshemalka2014@gmail.com>
2019-08-23 07:46:31 +0300

    finished ExRatesRetrieval function

--- Services/ExchangeRatesRetrieval.py
+++ Services/ExchangeRatesRetrieval.py
@@ -20,17 +20,17 @@ def handler(event, context):
         objects = s3_client.list_objects_v2(
             Bucket=os.environ['RATES_BUCKET'],
             Prefix=event['date']
-            )['Contents']
+            ).get('Contents', [])
     except ClientError as ce:
-        print(f"Error While trying to retrive Rates file with Date {event['date']}- {ce}")
+        print(f"Error While trying to retrive Rates file with Date {event['date']}. Error : {ce}")
         objects = []
     
-    # '2019-08-20/fixed.io/rates.json'
+    # e.g. '2019-08-20/fixed.io/rates.json'
     output = []
     for obj in objects:
-        broker_name = obj['key'].split('/')[1]
+        broker_name = obj['Key'].split('/')[1]
         try:
-            response = s3_client.get_object(Bucket=os.environ['RATES_BUCKET'], Key=obj['key'])
+            response = s3_client.get_object(Bucket=os.environ['RATES_BUCKET'], Key=obj['Key'])
             file_content = response['Body'].read().decode('utf-8') 
             json_content = json.loads(file_content)
         except ClientError as ce:
